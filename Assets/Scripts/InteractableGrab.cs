05e8647424cdfdba5eb6d045479242de75444c33
MatteoEtzer <matteoetzer@gmx.at>
2020-04-29 15:31:40 +0200

    mouse movement

---
+++ Assets/Scripts/InteractableGrab.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+	[RequireComponent( typeof( Interactable ) )]
+	public class InteractableGrab : MonoBehaviour
+    {
+        private Vector3 oldPosition;
+		private Quaternion oldRotation;
+
+		private float attachTime;
+
+		private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & ( ~Hand.AttachmentFlags.SnapOnAttach ) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+
+        private Interactable interactable;
+
+		void Awake()
+		{
+            interactable = this.GetComponent<Interactable>();
+		}
+
+        private void HandHoverUpdate(Hand hand)
+        {
+            GrabTypes startingGrabType = hand.GetGrabStarting();
+            bool isGrabEnding = hand.IsGrabEnding(this.gameObject);
+
+            if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+            {
+                // Save our position/rotation so that we can restore it when we detach
+                oldPosition = transform.position;
+                oldRotation = transform.rotation;
+
+                // Call this to continue receiving HandHoverUpdate messages,
+                // and prevent the hand from hovering over anything else
+                hand.HoverLock(interactable);
+
+                // Attach this object to the hand
+                hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+            }
+            else if (isGrabEnding)
+            {
+                // Detach this object from the hand
+                hand.DetachObject(gameObject);
+
+                // Call this to undo HoverLock
+                hand.HoverUnlock(interactable);
+
+                // Restore position/rotation
+                //transform.position = oldPosition;
+                //transform.rotation = oldRotation;
+            }
+        }
+
+        private bool lastHovering = false;
+        private void Update()
+        {
+            if (interactable.isHovering != lastHovering) //save on the .tostrings a bit
+            {
+                //hoveringText.text = string.Format("Hovering: {0}", interactable.isHovering);
+                lastHovering = interactable.isHovering;
+            }
+        }
+	}
+}
