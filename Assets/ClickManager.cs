56c70c80ad8849514be0ba675336687b6d359f09
matteoetzer@gmx.at <matteoetzer@gmx.at>
2020-06-24 21:32:34 +0200

    starting commit

---
+++ Assets/ClickManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    RaycastHit hit;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1000f))
+            {
+                Debug.Log("angeclickt");
+            }
+        }
+    }
+}
